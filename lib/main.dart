5dcc9dfb31ade5c738be08e71770a6608ea82630
a313 <nguyenthanhtung0207@gmail.com>
2022-07-06 10:58:46 +0700

    changed main, my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'injection.dart' as global;
 import 'my_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  global.setup();
   runApp(const MyApp());
 }
