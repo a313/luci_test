90af31311e832611c6ccc361bf3fed071747a2ec
tungnt <tungnt@techlab.ai>
2022-07-07 12:00:15 +0700

    removed test

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:luci_test/core/base/global_keys.dart';
-import 'package:luci_test/domain/usecases/get_user_list.dart';
 import 'package:luci_test/injection.dart';
 import 'package:luci_test/presentation/pages/user_detail/user_detail_page.dart';
 
@@ -11,16 +10,8 @@ import 'presentation/bloc/activity_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final GetUserList getUserList = getIt();
-    final test = getUserList.call(NoParams());
-    test.then((value) {
-      print(value);
-    });
-
     return MultiBlocProvider(
       providers: [
         BlocProvider(
